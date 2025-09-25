0a3cc3f48d9dfeb010cce5074f0909f57b58a229
Федащук Владислав bc2025-2 <vladfedaschuk2007@gmail.com>
2025-09-25 19:11:15 +0300

    feat: add urls (part I), NBU request (part II) and response, plus main.js (part III)

---
+++ main.js
@@ -0,0 +1,13 @@
+﻿const { URL } = require('url');
+
+const currency = 'JPY';
+const exact_date = '20240127';
+
+const nbBase = 'https://bank.gov.ua';
+const url = new URL('/NBUStatService/v1/statdirectory/exchange', nbBase);
+url.searchParams.set('valcode', currency);
+url.searchParams.set('date', exact_date);
+// зробимо &json без значення
+url.search = url.search + (url.search ? '&' : '') + 'json';
+
+console.log(url.toString());
